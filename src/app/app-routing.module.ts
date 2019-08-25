474348e352c103088c68d24a8039786aa0f614e6
WendyOmollo <omollow280@gmail.com>
2019-08-25 21:59:50 +0300

    Remove a bug that made the ngModel disfunctional

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,12 +2,13 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { RepoParentComponent } from './repo-parent/repo-parent.component';
 import { GithubComponent } from './github/github.component';
+import{ RepoComponent} from './repo/repo.component';
 
 const routes: Routes = [
   { path: 'github', component: GithubComponent},
-  { path: 'repo-parent', component: RepoParentComponent},
+  { path: 'repo', component: RepoComponent},
   ]
-  
+
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
--- src/app/repo/repo.component.ts
+++ src/app/repo/repo.component.ts
@@ -1,16 +1,13 @@
 import { Component, OnInit ,Output , EventEmitter} from '@angular/core';
-import { FormsModule} from '@angular/forms';
 
 @Component({
   selector: 'app-repo',
   templateUrl: './repo.component.html',
   styleUrls: ['./repo.component.css']
 })
-export class RepoComponent implements OnInit {
-
-  searchRepo:string;
+  export class RepoComponent implements OnInit {
   @Output() emitEvent = new EventEmitter<any>()
-
+  searchRepo:string;
 
   searchPerson(){
     this.emitEvent.emit(this.searchRepo);
